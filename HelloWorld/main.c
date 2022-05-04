72574879f33b193ab43bd193acd947fd5ecb69a3
jacklors <jackson.loredo.main@gmail.com>
2022-05-04 20:26:46 -0300

    Adicionado Algumas Atividades

---
+++ HelloWorld/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	int year;
+	
+	year = 2022;
+	
+	printf("Hello World! We are in the year %d.",year);
+
+
+	return 0;
+}
